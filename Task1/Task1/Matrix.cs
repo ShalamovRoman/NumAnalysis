6466a9a391355aed1b6d9caf99c534d493a448fb
ShalamovRoman <bringorbreak@yahoo.com>
2018-02-19 09:38:13 +0300

    add Task1

---
+++ Task1/Task1/Matrix.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Task1
+{
+    class Matrix
+    {
+        [Required]
+        private double[,] _matrix;
+        [Required]
+        public int Dimension
+        {
+            get
+            {
+                return _matrix.GetLength(0);
+
+            }
+        } //Размерность матрицы
+        public double Determinant
+        {
+            get
+            {
+                return GetDeterminant(_matrix);
+            }
+        } //Определитель матрицы
+        public double Norm {
+            get
+            {
+                return GetNorm(_matrix);
+            }
+        } //Норма один
+
+        public double this[int i, int j]
+        {
+            get
+            {
+                return _matrix[i, j];
+            }
+            set
+            {
+                _matrix[i, j] = value;
+            }
+        }
+
+        /// <summary>
+        /// Converts array in Matrix
+        /// </summary>
+
+        public Matrix(double[,] arr)
+        {
+            _matrix = arr;
+            
+        }
+
+        /// <summary>
+        /// Creates square matrix
+        /// </summary>
+        public Matrix(int n)
+        {
+            _matrix = new double[n, n];
+        }
+
+        double GetDeterminant(double[,] _matrix)
+        {
+            if (_matrix.GetLength(0) == 2)
+            {
+                return _matrix[0, 0] * _matrix[1, 1] - _matrix[0, 1] * _matrix[1, 0];
+            }
+            else if (_matrix.GetLength(0) == 1)
+            {
+                return _matrix[0, 0];
+            }
+            double sign = 1, result = 0;
+            for (int i = 0; i < _matrix.GetLength(0); i++)
+            {
+                double[,] minor = GetMinor(_matrix, i);
+                result += sign * _matrix[0, i] * GetDeterminant(minor);
+                sign = -sign;
+            }
+            return result;
+        }
+
+        static double[,] GetMinor(double[,] _matrix, int n)
+        {
+            double[,] result = new double[_matrix.GetLength(0) - 1, _matrix.GetLength(0) - 1];
+
+            for (int i = 1; i < _matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < n; j++)
+                    result[i - 1, j] = _matrix[i, j];
+
+                for (int j = n + 1; j < _matrix.GetLength(0); j++)
+                    result[i - 1, j - 1] = _matrix[i, j];
+            }
+            return result;
+        }
+
+        double GetNorm(double[,] _matrix)
+        {
+            double res = 0,sum = 0;
+            for (int i = 0; i < _matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < _matrix.GetLength(0); j++)
+                    sum += Math.Abs(_matrix[i, j]);
+
+                if (sum >= res) res = sum;
+                sum = 0;
+            }
+
+            return res;
+        }
+
+        public static Matrix GetInvertibleMatrix(double[,] _matrix)
+        {
+            Matrix minorMatrix = new Matrix(_matrix.GetLength(0));
+            double sign = 1;
+            //Вычисление элементов
+            for (int i = 0; i < _matrix.GetLength(0); i++)
+            for (int j = 0; j < _matrix.GetLength(0); j++)
+            {
+                double[,] minor = GetMinor(_matrix, j);
+                Matrix minorMatr = new Matrix(minor);
+                PrintMatrix(minorMatr);
+                minorMatrix[i, j] = minorMatr.Determinant*sign;
+                sign = -sign;
+            }
+
+            minorMatrix = TransposeMatrix(minorMatrix);
+            Matrix m = new Matrix(_matrix);
+            for (int i = 0; i < minorMatrix.Dimension; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    minorMatrix[i, j] = minorMatrix[i, j] / m.Determinant;
+                }
+            }
+
+            return minorMatrix;
+        }
+
+        public static Matrix TransposeMatrix(Matrix matrix)
+        {
+            double tmp = 0;
+            for (int i = 0; i < matrix.Dimension; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    tmp = matrix[i, j];
+                    matrix[i, j] = matrix[j, i];
+                    matrix[j, i] = tmp;
+                }
+            }
+
+            return matrix;
+        }
+
+        public static Matrix CreateHilbertMatrix(int n)
+        {
+            Matrix matrixA = new Matrix(n);
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    matrixA[i - 1, j - 1] = 1 / (double)(i + j - 1);
+                }
+            }
+        return matrixA;
+        }
+
+        public static double[,] ToArray(Matrix _matrix)
+        {
+            double[,] arr = new double[_matrix.Dimension, _matrix.Dimension];
+            for (int i = 0; i < _matrix.Dimension; i++)
+            for (int j = 0; j < _matrix.Dimension; j++)
+                arr[i, j] = _matrix[i, j];
+
+            return arr;
+        }
+
+        public static void PrintMatrix(Matrix matrix)
+        {
+            for (int i = 0; i < matrix.Dimension; i++)
+            {
+                for (int j = 0; j < matrix.Dimension; j++)
+                {
+                    Console.Write("{0:f2}  ", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace Task1
+{
+    class Program
+    {
+        //Создание вектора b из случайных чисел
+        static double[] CreateRandomVector(int n)
+        {
+            double[] Vector = new double[n];
+            Random Rnd = new Random();
+            for (int i = 0; i < Vector.Length; i++)
+            {
+                Vector[i] = Rnd.NextDouble() + Rnd.Next(-5, 5);
+            }
+            return Vector;
+        }
+
+        //Метод крамера
+        static double[] CramersRule(Matrix matrix, double[] vector)
+        {
+            double[] res = new double[vector.Length];
+
+            Matrix tmpMatrix = new Matrix(vector.Length);
+            for (int i = 0; i < vector.Length; i++)
+            {
+                for (int j = 0; j < tmpMatrix.Dimension; j++)
+                for (int k = 0; k < tmpMatrix.Dimension; k++)
+                    tmpMatrix[j, k] = matrix[j, k];
+                tmpMatrix = ChangeVector(tmpMatrix, vector, i);
+                res[i] = tmpMatrix.Determinant;
+                //Matrix.PrintMatrix(tmpMatrix)
+            }
+            return res;
+        }
+
+        //Метод замены одного столбца матрицы на заданный вектор
+        static Matrix ChangeVector(Matrix matrix, double[] vector, int column)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                matrix[i, column] = vector[i];
+            }
+            return matrix;
+
+        }
+
+        static double VectorNorm(double[] vector)
+        {
+            double res = 0;
+            for (int i = 0; i < vector.Length; i++)
+                res += Math.Abs(vector[i]);
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размерность матрицы: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nМатрица А: \n");
+            var A = Matrix.CreateHilbertMatrix(n);
+            Matrix.PrintMatrix(A);
+
+            Console.WriteLine("Вектор b: \n");
+            double[] b = CreateRandomVector(n);
+            foreach (double j in b)
+                Console.Write("{0:0.00}  ", j);
+
+            Console.WriteLine("\n\nОпределитель матрицы A = {0}", A.Determinant);
+
+            Console.WriteLine("\nРешения уравнения Ax = b:\n");
+            double[] x = new double[b.Length];
+            x = CramersRule(A, b);
+            for (int i = 0; i < x.Length; i++)
+                Console.WriteLine("x{0} = {1:0.00}", i, x[i]);
+
+            Console.WriteLine("\nПриращение A: \n");
+            Matrix newA = new Matrix(A.Dimension);
+            Random rnd = new Random();
+            for (int i = 0; i < A.Dimension; i++)
+            for (int j = 0; j < A.Dimension; j++)
+                    newA[i,j] = A[i,j] + rnd.NextDouble();
+            Matrix.PrintMatrix(newA);
+
+            Console.WriteLine("\nПриращение b: \n");
+            double[] newb = new double[b.Length];
+            for (int i = 0; i < b.Length; i++)
+                newb[i] = b[i] + rnd.NextDouble(); ;
+
+            foreach (double j in newb)
+                Console.Write("{0:0.00}  ", j);
+
+            Console.WriteLine("\n\nРешение нового уравнения: \n");
+            double[] x1 = new double[b.Length];
+            x1 = CramersRule(newA,newb);
+
+            for (int i = 0; i < x.Length; i++)
+                Console.WriteLine("x*{0} = {1:0.00}", i, x1[i]);
+
+            Console.WriteLine("\n\nДельта x: \n");
+            double[] newx = new double[b.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                newx[i] = Math.Abs(x[i] - x1[i]);
+                Console.WriteLine("x*{0} = {1:0.00}", i, newx[i]);
+            }
+
+            Console.WriteLine("\nОбратная матрица к А: \n");
+
+            Matrix<double> a = Matrix<double>.Build.DenseOfArray(Matrix.ToArray(A)); 
+            a = a.Inverse();
+            //invertA = Matrix.GetInvertibleMatrix(Matrix.ToArray(A));
+            var invertA = new Matrix(a.ToArray()); 
+            Matrix.PrintMatrix(invertA);
+
+            Console.Write("||A|| = {0:0.00}\n ", A.Norm);
+            Console.Write("\n||A^-1|| = {0:0.00}\n ", invertA.Norm);
+            Console.Write("\ncond(A) = {0}\n", A.Norm/invertA.Norm);
+
+            Console.Write("\n||x|| = {0}\n ", VectorNorm(x));
+            Console.Write("\n||x*|| = {0}\n ", VectorNorm(newx));
+            Console.Write("\n||x|| / ||x*|| = {0}", VectorNorm(x) / VectorNorm(newx));
+
+            double d = (A.Norm / invertA.Norm) / (1 - A.Norm / invertA.Norm * newA.Norm / A.Norm) *
+                       (VectorNorm(newb) / VectorNorm(b) + newA.Norm / A.Norm);
+            Console.WriteLine("\n\nОценка = {0}", d);
+            Console.ReadLine();
+        
+        }
+
+
+    }
+}
