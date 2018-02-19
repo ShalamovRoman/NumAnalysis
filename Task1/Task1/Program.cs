951b923f7d6eb90f2588cf164b2f5185db2b130d
ShalamovRoman <bringorbreak@yahoo.com>
2018-02-19 10:27:26 +0300

    update

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -35,7 +35,8 @@ namespace Task1
                 for (int k = 0; k < tmpMatrix.Dimension; k++)
                     tmpMatrix[j, k] = matrix[j, k];
                 tmpMatrix = ChangeVector(tmpMatrix, vector, i);
-                res[i] = tmpMatrix.Determinant;
+                res[i] = tmpMatrix.Determinant/matrix.Determinant;
+                
                 //Matrix.PrintMatrix(tmpMatrix)
             }
             return res;
@@ -80,9 +81,9 @@ namespace Task1
             double[] x = new double[b.Length];
             x = CramersRule(A, b);
             for (int i = 0; i < x.Length; i++)
-                Console.WriteLine("x{0} = {1:0.00}", i, x[i]);
+                Console.WriteLine("x{0} = {1}", i, x[i]);
 
-            Console.WriteLine("\nПриращение A: \n");
+            Console.WriteLine("\nНовая матрица A: \n");
             Matrix newA = new Matrix(A.Dimension);
             Random rnd = new Random();
             for (int i = 0; i < A.Dimension; i++)
@@ -90,7 +91,14 @@ namespace Task1
                     newA[i,j] = A[i,j] + rnd.NextDouble();
             Matrix.PrintMatrix(newA);
 
-            Console.WriteLine("\nПриращение b: \n");
+            Console.WriteLine("\nПриращение A: \n");
+            Matrix deltaA = new Matrix(A.Dimension);
+            for (int i = 0; i < A.Dimension; i++)
+            for (int j = 0; j < A.Dimension; j++)
+                deltaA[i, j] = Math.Abs(A[i, j] - newA[i,j]);
+            Matrix.PrintMatrix(deltaA);
+
+            Console.WriteLine("\nНовый вектор b: \n");
             double[] newb = new double[b.Length];
             for (int i = 0; i < b.Length; i++)
                 newb[i] = b[i] + rnd.NextDouble(); ;
@@ -98,12 +106,20 @@ namespace Task1
             foreach (double j in newb)
                 Console.Write("{0:0.00}  ", j);
 
+            Console.WriteLine("\n\nПриращение b: \n");
+            double[] deltab = new double[b.Length];
+            for (int i = 0; i < b.Length; i++)
+                deltab[i] = Math.Abs(b[i] - newb[i]); ;
+
+            foreach (double j in deltab)
+                Console.Write("{0:0.00}  ", j);
+
             Console.WriteLine("\n\nРешение нового уравнения: \n");
             double[] x1 = new double[b.Length];
             x1 = CramersRule(newA,newb);
 
             for (int i = 0; i < x.Length; i++)
-                Console.WriteLine("x*{0} = {1:0.00}", i, x1[i]);
+                Console.WriteLine("x*{0} = {1}", i, x1[i]);
 
             Console.WriteLine("\n\nДельта x: \n");
             double[] newx = new double[b.Length];
@@ -123,15 +139,17 @@ namespace Task1
 
             Console.Write("||A|| = {0:0.00}\n ", A.Norm);
             Console.Write("\n||A^-1|| = {0:0.00}\n ", invertA.Norm);
-            Console.Write("\ncond(A) = {0}\n", A.Norm/invertA.Norm);
+            Console.Write("\ncond(A) = {0}\n", A.Norm*invertA.Norm);
 
             Console.Write("\n||x|| = {0}\n ", VectorNorm(x));
             Console.Write("\n||x*|| = {0}\n ", VectorNorm(newx));
-            Console.Write("\n||x|| / ||x*|| = {0}", VectorNorm(x) / VectorNorm(newx));
+            Console.Write("\n||x|| / ||x*|| = {0}", VectorNorm(newx) / VectorNorm(x));
 
-            double d = (A.Norm / invertA.Norm) / (1 - A.Norm / invertA.Norm * newA.Norm / A.Norm) *
-                       (VectorNorm(newb) / VectorNorm(b) + newA.Norm / A.Norm);
+            double d = (A.Norm * invertA.Norm) / (1 - invertA.Norm * deltaA.Norm) *
+                       (VectorNorm(deltab) / VectorNorm(b) + deltaA.Norm / A.Norm);
             Console.WriteLine("\n\nОценка = {0}", d);
+
+            Console.WriteLine(invertA.Norm * deltaA.Norm);
             Console.ReadLine();
         
         }
